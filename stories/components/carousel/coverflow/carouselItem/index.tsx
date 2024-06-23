b55bfd86b17de9e8b90b9ba72edcf6a7fad55a7c
Juanjo <juanjo.desarrollador@gmail.com>
2024-06-23 19:33:54 -0400

    actualizando coverflowCarousel

--- stories/components/carousel/coverflow/carouselItem/index.tsx
+++ stories/components/carousel/coverflow/carouselItem/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { CoverflowCarouselItemProps } from "../../../../interfaces/carousel/coverflowCarousel";
 import { SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 export const CoverflowCarouselItem = ({
   children,
   itemWidth = 100,
