182c82f38797675fea4ac2013176e3e64a8dc101
Juanjo <juanjo.desarrollador@gmail.com>
2024-11-10 01:48:18 -0300

    actualizando diseño side menu

--- src/widgets/navbar/sideNavBar/index.jsx
+++ src/widgets/navbar/sideNavBar/index.jsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 import { TbLayoutNavbarFilled } from "react-icons/tb";
 import "./style/style.scss";
-export const SideNavBar = () => {
+export const SideNavBar = ({
+  primaryColor = "#00b3ff",
+  secondaryColor = "#a5ffff",
+}) => {
   return (
     <>
-      <div className="navigation">
+      <div
+        className="navigation"
+        style={{
+          "--secondaryColor": secondaryColor,
+          "--primaryColor": primaryColor,
+        }}
+      >
         <Link
           to="/"
-          className="text-decoration-none text-dark d-flex flex-row navigation-item mb-2"
+          className="text-decoration-none d-flex flex-row navigation-item py-2"
         >
           <div>
             <span className="navigation-item-icon rounded rounded-circle d-flex justify-content-center align-items-center">
@@ -20,14 +29,27 @@ export const SideNavBar = () => {
         </Link>
         <Link
           to="/"
-          className="text-decoration-none text-dark d-flex flex-row navigation-item"
+          className="text-decoration-none d-flex flex-row navigation-item py-2"
         >
           <div>
             <span className="navigation-item-icon rounded rounded-circle d-flex justify-content-center align-items-center">
               <TbLayoutNavbarFilled size={24} />
             </span>
           </div>
-          <span className="navigation-item-text my-auto pe-2">
+          <span className="navigation-item-text my-auto ">
+            dsfadsfdsdf dsfdsaf
+          </span>
+        </Link>
+        <Link
+          to="/"
+          className="text-decoration-none d-flex flex-row navigation-item py-2"
+        >
+          <div>
+            <span className="navigation-item-icon rounded rounded-circle d-flex justify-content-center align-items-center">
+              <TbLayoutNavbarFilled size={24} />
+            </span>
+          </div>
+          <span className="navigation-item-text my-auto ">
             dsfadsfdsdf dsfdsaf
           </span>
         </Link>
